0fcb1495f53b41529c4681cfe77520a4d0c151b7
Grégoire Détrez <gdetrez@crans.org>
2011-07-03 21:05:08 +0000

    Cleaner configuration, local conf is moved to a different, utracked file.

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Django settings for myconf project.
+from localsettings import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -46,9 +47,6 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/gdetrez/Projects/MyConf/myconf/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -80,12 +78,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'myconf.urls'
 
-TEMPLATE_DIRS = (
-    "/home/gdetrez/Projects/MyConf/myconf/templates"
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
