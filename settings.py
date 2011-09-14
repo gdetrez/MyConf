250430ac64f4dc8d7d0af3c687542e6fe0b8f4b4
Grégoire Détrez <gdetrez@crans.org>
2011-09-14 13:12:14 +0200

    Adding markdown support in session description

--- settings.py
+++ settings.py
@@ -88,6 +88,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    # Add markup
+    'django.contrib.markup',
     
     # MyConf apps
     'myconf.people',
