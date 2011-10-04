abb13f1cbd25ecf2d945092781c11ac23945d3d7
Grégoire Détrez <gdetrez@crans.org>
2011-10-04 10:07:58 +0200

    adding 404 page

---
+++ templates/404.php
@@ -0,0 +1,10 @@
+{% extends "base.djhtml" %}
+{% block head-title %}404 — Page not fount{% endblock %}
+
+{% block content %}
+<h2>404 — Page not fount</h2>
+<span class="subtitle">You are here ➞ ⦿</span>
+
+<p>Ouups, seems like you got lost. Sorry about that. Would you like to get back to the <a href="/">front page</a>?</p>
+
+{% endblock %}
