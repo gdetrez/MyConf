c6f1b946feb0287b7b4f8005f183c2b9cc6a8bda
Grégoire Détrez <gdetrez@crans.org>
2011-10-04 11:50:46 +0200

    Using Fabric for deployment

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from fabric.api import *
+
+env.hosts = ['fscons.org']
+
+def deploy():
+    local("git push")
+    code_dir = '/var/django/myconf'
+    with cd(code_dir):
+        run("git pull")
+        run("python manage.py collectstatic --noinput")
+        sudo("touch ../wsgi/django.wsgi")
