5a7215ebfcaa5eb1a82e61a45f56eab5b8ccc676
Grégoire Détrez <gdetrez@crans.org>
2011-10-17 20:06:20 +0200

    Adding a new task for refreshing the local data

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import *
 
 env.hosts = ['fscons.org']
 
+@task(default=True)
 def deploy():
     local("git push")
     code_dir = '/var/django/myconf'
@@ -9,3 +10,11 @@ def deploy():
         run("git pull github master")
         run("python manage.py collectstatic --noinput")
         sudo("/etc/init.d/apache2 restart")
+
+@task(alias="refresh")
+def refresh_initial_data():
+    code_dir = '/var/django/myconf'
+    with cd(code_dir):
+        run("python manage.py dumpdata signup schedule people restaurants > initial_data.json")
+        get("initial_data.json", ".")
+
