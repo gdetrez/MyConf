db89eb031bb9a4f51496f3baccc38241ebc93642
Grégoire Détrez <gdetrez@crans.org>
2011-10-18 16:59:51 +0200

    Adding "Lightning talks" as a session kind

--- schedule/models.py
+++ schedule/models.py
@@ -75,13 +75,7 @@ SESSION_KIND_CHOICES = (
     ('T', 'Talk'),
     ('W', 'Workshop'),
     ('K', 'Keynote'),
-    ('O', 'Other'),
-)
-
-SESSION_KIND_CHOICES = (
-    ('T', 'Talk'),
-    ('W', 'Workshop'),
-    ('K', 'Keynote'),
+    ('L', 'Lightning talkssc'),
     ('O', 'Other'),
 )
 
