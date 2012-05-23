db9931a40c5335e3f7e6e68e0250ae596e41d828
Grégoire Détrez <gdetrez@crans.org>
2012-05-23 19:18:58 +0200

    Updating the deployment routine for the new server

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,45 @@
 from fabric.api import *
 from datetime import datetime
-env.hosts = ['fscons.org']
+from fabric.contrib import files, console
+from fabric.contrib.project import rsync_project
+from fabric import utils
+import os
+
+env.hosts = ['178.79.156.98']
+env.user = 'gdetrez'
+env.root = '/srv/fscons.org'
+env.virtualenv_root = '/srv/fscons.org/env'
+
+RSYNC_EXCLUDE = (
+    '.DS_Store',
+    '.git',
+    'env',
+    '*.pyc',
+    '*.db',
+    '*~',
+    'local_settings.py',
+    'fabfile.py',
+    'bootstrap.py',
+)
 
 @task(default=True)
 def deploy():
-    local("git push zjyto")
-    code_dir = '/var/django/myconf'
-    with cd(code_dir):
-        run("git pull origin master")
-        run("python manage.py collectstatic --noinput")
-        sudo("/etc/init.d/apache2 restart")
+    """ rsync code to remote host """
+    if not console.confirm('Are you sure you want to deploy production?',
+                           default=False):
+        utils.abort('Production deployment aborted.')
+    extra_opts = '--omit-dir-times'
+    rsync_project(
+        env.root,
+        exclude=RSYNC_EXCLUDE,
+        delete=True,
+        extra_opts=extra_opts,
+    )
+    touch()
 
-@task(alias="refresh")
-def refresh_initial_data():
-    code_dir = '/var/django/myconf'
-    fname = "initial_data_%s.json" % datetime.now().strftime("%Y%m%d%H%M")
-    with cd(code_dir):
-        run("python manage.py dumpdata signup schedule people restaurants > %s" % fname)
-        get(fname, "fixtures")
-
+def touch():
+    """ touch wsgi file to trigger reload """
+    #require('code_root', provided_by=('staging', 'production'))
+    apache_dir = os.path.join(env.root, 'myconf', 'apache')
+    with cd(apache_dir):
+        run('touch staging.wsgi')
