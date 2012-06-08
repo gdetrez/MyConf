f8353dc49e00d3791b4d0927710f67594f5fa5c8
Grégoire Détrez <gdetrez@crans.org>
2012-06-08 12:12:35 +0200

    Adding automatic run of collectstatic when deploying

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,15 @@ from datetime import datetime
 from fabric.contrib import files, console
 from fabric.contrib.project import rsync_project
 from fabric import utils
+from fabric.api import env, require, run, sudo, cd
 import os
 
-env.hosts = ['178.79.156.98']
+env.hosts = ['fscons.org']
 env.user = 'gdetrez'
 env.root = '/srv/fscons.org'
-env.virtualenv_root = '/srv/fscons.org/env'
+env.project_root = os.path.join(env.root, 'myconf')
+env.virtualenv_root = os.path.join(env.root, 'env')
+env.activate_script = os.path.join(env.virtualenv_root, 'bin','activate')
 
 RSYNC_EXCLUDE = (
     '.DS_Store',
@@ -24,10 +27,33 @@ RSYNC_EXCLUDE = (
 
 @task(default=True)
 def deploy():
-    """ rsync code to remote host """
     if not console.confirm('Are you sure you want to deploy production?',
                            default=False):
         utils.abort('Production deployment aborted.')
+    rsync()
+    collectstatic()
+    reload()
+
+
+def virtualenv(command, use_sudo=False):
+    if use_sudo:
+        func = sudo
+    else:
+        func = run
+    func('source "%s" && %s' % (env.activate_script, command))
+
+def manage_py(command, use_sudo=False):
+    with cd(env.project_root):
+        virtualenv('python manage.py %s' % command, use_sudo)
+
+@task
+def collectstatic():
+    #require('hosts', provided_by=[production])
+    manage_py('collectstatic -l --noinput')
+
+@task
+def rsync():
+    """ rsync code to remote host """
     extra_opts = '--omit-dir-times'
     rsync_project(
         env.root,
@@ -35,11 +61,12 @@ def deploy():
         delete=True,
         extra_opts=extra_opts,
     )
-    touch()
 
-def touch():
+@task
+def reload():
     """ touch wsgi file to trigger reload """
-    #require('code_root', provided_by=('staging', 'production'))
     apache_dir = os.path.join(env.root, 'myconf', 'apache')
     with cd(apache_dir):
         run('touch staging.wsgi')
+
+
